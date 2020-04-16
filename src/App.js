29a83dcb6cb9750b503e2e764f07a05ec1df1373
catcarbonell <hello@catcarbn.com>
2020-04-15 17:43:18 -0700

    Adding redux stuff

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import React from 'react';
 import Nav from './layout/Nav';
 // import Footer from './layout/Footer';
 import Routes from './config/Routes';
+
+// REDUX
+import { Provider } from 'react-redux';
+import store from './store';
+
+// CSS
 import './bulma.css';
 import './index.css';
 // Maintain State here
 class App extends React.Component{
-
-  state = {};
-
   render(){
     return (
-      <div className="App-content">
-        <Nav />
-        <Routes />
-        {/* <Footer /> */}
-      </div>    
+        <Provider store={store}>
+        <div className="App-content">
+          <Nav />
+          <Routes />
+          {/* <Footer /> */}
+        </div>
+      </Provider>
     
     );
   }
